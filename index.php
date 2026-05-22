a7cfa66e7284f508a01884c223055ec0ee6f6cb3
Robbialbert87 <robbialbert@gmail.com>
2026-05-22 12:06:05 +0700

    chore: add empty line after session start in index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_start();
+session_start(); 
+
 
 // 2. Autoloader
 spl_autoload_register(function ($class) {
